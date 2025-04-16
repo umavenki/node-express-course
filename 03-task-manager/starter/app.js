f7c918586e412974bc3a5a5b17f50a7da7a08065
Uma Venki <iamumamsekar@gmail.com>
2025-04-16 09:09:51 -0700

    Week5 assignment

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,27 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
+app.use(express.json());
+
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen("3000", () => {
+      console.log("Server started");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
+
+// console.log("Task Manager App");
